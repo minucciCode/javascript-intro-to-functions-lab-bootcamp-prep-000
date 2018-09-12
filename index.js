af281a24921788ed03ae4a112b7dfeaa13726e89
Gianluca Minucci <glminucci@gmail.com>
2018-09-12 21:27:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,18 @@ function logWhisper(string)
   console.log(string.toLowerCase())
 }
 
-function sayHitoGrandma(string){
+function sayHitoGrandma(string)
+{
   if (string.toLowerCase() === string) 
+  {
   return "I can't hear you!"
+  }
   else if (string.toUpperCase() === string)
-  return "YES INDEED!"
+  {
+    return "YES INDEED!"
+  }
   else if (string() === "I love you, Grandma.")
-  return "I love you, too."
+ {
+   return "I love you, too."
+ }
 }
